591c376858fbb097d059c22495949c4254e8c8af
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2019-01-21 15:19:23 -0600

    Iniciando Dal

---
+++ GameOverOnlineSolution/Comun/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun
+{
+    class Cliente
+    {
+        #region atributos
+        /// <summary>
+        /// Nombre del paciente
+        /// </summary>
+        public int ClienteId { get; set; }
+
+        /// <summary>
+        /// Nombre del paciente
+        /// </summary>
+        public string Nombre { get; set; }
+
+        /// <sumary>
+        /// Apellido del cliente
+        /// </sumary>
+        public string Apellido { get; set; }
+        /// <summary>
+        /// Sexo del paciente
+        /// </summary>
+        
+        /// <summary>
+        /// Correo electronico del cliente
+        /// </summary>
+        public string CorreoElectronico { get; set; }
+
+        /// <summary>
+        /// Recupera la informacion del password mediante in ID del mismo
+        /// </summary>
+        /// 
+        #endregion
+    }
+}
