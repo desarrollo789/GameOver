5b2f868b1cb4517c65af91d0f42902c0f81e9b54
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2019-01-22 16:26:07 -0600

    Crearcion de la clase password

--- GameOverOnlineSolution/Comun/Password.cs
+++ GameOverOnlineSolution/Comun/Password.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Comun
 {
-    class Password
+    public static class Password
     {
-
+        /// <summary>
+        /// Esta funcion encripta la cadena
+        /// </summary>
+        public static string Encriptar(this string _cadenaAencriptar)
+        {
+            string result = string.Empty;
+            byte[] encrypted = System.Text.Encoding.Unicode.GetBytes(_cadenaAencriptar);
+            result = Convert.ToBase64String(encrypted);
+            return result;
+        }
+        /// <summary>
+        /// Esta funcion desencripta la cadena.
+        /// </summary>
+        public static string DesEncriptar(this string _cadenaAdesencriptar)
+        {
+            string result = string.Empty;
+            byte[] decryted = Convert.FromBase64String(_cadenaAdesencriptar);
+            result = System.Text.Encoding.Unicode.GetString(decryted);
+            return result;
+        }
     }
 }
