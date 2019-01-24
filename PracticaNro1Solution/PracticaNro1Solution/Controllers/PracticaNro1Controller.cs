0a52647df986c3af74b45669a16dab3021a57273
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2019-01-24 07:59:09 -0600

    Bajando Proyecto

---
+++ PracticaNro1Solution/PracticaNro1Solution/Controllers/PracticaNro1Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PracticaNro1Solution.Models;
+
+namespace PracticaNro1Solution.Controllers
+{
+    public class PracticaNro1Controller : Controller
+    {
+        // GET: PracticaNro1
+        //Get: Ejercicio nro 1
+        public ActionResult Ejecicio1()
+        {
+            EjercicioNro1 ejercicio1 = new EjercicioNro1();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ejecicio1(EjercicioNro1 model)
+        {
+            model.respuesta = (int)Math.Pow(model.numero_a, model.numero_b);
+            return View(model);
+        }
+        // 
+    }
+}
---
+++ PracticaNro1Solution/PracticaNro1Solution/Models/EjercicioNro1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticaNro1Solution.Models
+{
+    public class EjercicioNro1
+    {
+        /// <summary>
+        /// Obtiene  y controla el numero B
+        /// </summary>
+        [Required]
+        [Display(Name = "numero_a")]
+        [Range(0, int.MaxValue, ErrorMessage = "El numero debe ser entero positivo")]
+        public int numero_a { get; set; }
+
+        /// <summary>
+        /// Obtiene  y controla el numero B
+        /// </summary>
+        [Required]
+        [Display(Name = "numero_b")]
+        [Range(0, int.MaxValue, ErrorMessage = "El numro bede ser entero positivo")]
+        public int numero_b { get; set; }
+
+        public int respuesta { get; set; }
+    }
+}
