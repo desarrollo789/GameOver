590f07e2ad4553715995c1aa4b6e113536495000
GhostSergal <44811028+GhostSergal@users.noreply.github.com>
2019-01-24 09:58:21 -0400

    Proveedor,Videojuego,modificar Cliente

---
+++ GameOverOnlineSolution/Comun/Proveedor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun
+{
+    class Proveedor
+    {
+        #region atributos
+        /// <summary>
+        /// Id del Proveedor
+        /// </summary>
+        public int ProveedorId { get; set; }
+
+        /// <summary>
+        /// Correo Electronico del Proveedor
+        /// </summary>
+        public string CorreoElectronico { get; set; }
+
+        /// <summary>
+        /// Nombre del Proveedor
+        /// </summary>
+        public string NombreProveedor { get; set; }
+
+
+        /// <summary>
+        /// Apellido del Proveedor
+        /// </summary>
+
+        public string ApellidoProveedor { get; set; }
+        #endregion
+    }
+}
---
+++ GameOverOnlineSolution/Comun/VideoJuego.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun
+{
+    class VideoJuego
+    {
+        #region atributos
+        /// <summary>
+        /// Id del Videojuego
+        /// </summary>
+        public int VideoJuegoId { get; set; }
+
+        /// <summary>
+        /// Titulo del Videojuego
+        /// </summary>
+        public string VideoJuegoNombre { get; set; }
+
+        /// <summary>
+        /// Precio del Videojuego
+        /// </summary>
+        public float Precio { get; set; }
+
+
+        /// <summary>
+        /// Portada del Videojuego
+        /// </summary>
+        
+        public string Portada { get; set; }
+
+
+        /// <summary>
+        /// Trailer del Videojuego
+        /// </summary>
+        public string Trailer { get; set; }
+
+        ///<summary>
+        ///Detalles Tecnicos del Vieojuego
+        ///</summary>
+        public string DetalleTecnico { get; set; }
+
+        #endregion
+    }
+}
