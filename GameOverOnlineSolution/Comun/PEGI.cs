0c0728efad9308f88216a7fc08728d9ce83b2fad
GhostSergal <44811028+GhostSergal@users.noreply.github.com>
2019-01-25 11:52:08 -0400

    PEGI,Categoria,Proveedor

---
+++ GameOverOnlineSolution/Comun/PEGI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun
+{
+    class PEGI
+    {
+        #region Atributos
+        ///<summary>
+        ///Id para PEGI (rango de edades)
+        ///</summary>
+        public int PegiId { get; set; }
+
+        ///<summary>
+        ///Nomenclatura de PEGI
+        ///</summary>
+        public string Edad { get; set; }
+        #endregion
+    }
+}
