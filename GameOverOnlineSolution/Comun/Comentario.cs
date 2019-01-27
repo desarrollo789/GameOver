6ff0cf18b09e27fb54c7dd00e555d940e61548f2
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2019-01-26 23:03:32 -0600

    aumento de elementos del comun

---
+++ GameOverOnlineSolution/Comun/Comentario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun
+{
+    /// <summary>
+    /// Define los comentarios para un juego
+    /// </summary>
+    public class Comentario
+    {
+        /// <summary>
+        /// Identificador de comentario
+        /// </summary>
+        public int ComentarioId { get; set; }
+        /// <summary>
+        /// Identificador de usuario para un comentario
+        /// </summary>
+        public int UsuarioId { get; set; }
+        /// <summary>
+        /// Texto del comentario
+        /// </summary>
+        public string Comentariotext { get; set; }
+    }
+}
