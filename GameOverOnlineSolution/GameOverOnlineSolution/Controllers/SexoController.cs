f2ecfeb50dfb846771b60c4b93781d59180aeac8
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2019-01-28 15:49:54 -0600

    relaciones

---
+++ GameOverOnlineSolution/GameOverOnlineSolution/Controllers/SexoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using GameOverOnlineSolution.Models;
+using ClienteBrl;
+
+namespace GameOverOnlineSolution.Controllers
+{
+    public class SexoController
+    {
+        private static List<SexoModel> sexoList;
+        private static List<SexoModel> ListaSexo
+        {
+            get {
+                    sexoList = new List<SexoModel>();
+                foreach (var sexo in )
+                }
+        }
+    }
+}
