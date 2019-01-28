5925a938ae2463065ca1c764e8968629e88b0ebb
GhostSergal <44811028+GhostSergal@users.noreply.github.com>
2019-01-28 10:47:22 -0400

    SexoListController, SexoModel

---
+++ GameOverOnlineSolution/GameOverOnlineSolution/Controllers/SexoListController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameOverOnlineSolution.Controllers
+{
+    public class SexoListController : Controllers
+    {
+        private static List<SexoModel> _sexoModel;
+
+        public static List<SexoModel> SexoModel;
+    }
+}
---
+++ GameOverOnlineSolution/GameOverOnlineSolution/Models/SexoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameOverOnlineSolution.Models
+{
+    public class SexoModel
+    {
+        #region Propiedades
+        public byte SexoId
+        {
+            get;
+            set;
+        }
+        public string Nombre
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
