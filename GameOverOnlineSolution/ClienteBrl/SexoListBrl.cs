9cae3c53f584c5f2b449139bc5a305770252cdab
GhostSergal <44811028+GhostSergal@users.noreply.github.com>
2019-01-28 11:03:49 -0400

    SexoListBrl

---
+++ GameOverOnlineSolution/ClienteBrl/SexoListBrl.cs
@@ -0,0 +1,17 @@
+﻿using Comun;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteBrl
+{
+    public class SexoListBrl
+    {
+        public static SexoList Get()
+        {
+            return ClienteDal.SexoListDal.Get();
+        }
+    }
+}
