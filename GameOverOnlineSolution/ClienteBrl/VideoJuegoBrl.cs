d714ee948a3580df84ff5ab68d7036ac6227f062
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2019-01-28 09:03:23 -0600

    Videojuego Brl

--- GameOverOnlineSolution/ClienteBrl/VideoJuegoBrl.cs
+++ GameOverOnlineSolution/ClienteBrl/VideoJuegoBrl.cs
@@ -32,5 +32,69 @@ namespace ClienteBrl
         /// Eliminar Paciente
         /// </summary>
         /// <param name="id">Identificador del videojuego</param>
+        public static void Eliminar(int id)
+        {
+            try
+            {
+                VideoJuegoDal.Eliminar(id);
+            }
+            catch (SqlException ex)
+            {
+                Methods.GenerateLogsRelease("VideoJuegoBrl", "Eliminar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Methods.GenerateLogsRelease("VideoJuegoBrl", "Eliminar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Actualiza los datos del videojuego en la base de datos
+        /// </summary>
+        /// <param name="videojuego"></param>
+        public static void Actualizar(VideoJuego videojuego)
+        {
+            try
+            {
+                VideoJuegoDal.Actualizar(videojuego);
+            }
+            catch (SqlException ex)
+            {
+                Methods.GenerateLogsRelease("VideoJuegoBrl", "Actualizar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Methods.GenerateLogsRelease("VideoJuegoBrl", "Actualizar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Obtine  la informacion de un videojuego
+        /// </summary>
+        /// <param name="id">identificador del videojuego</param>
+        /// <returns></returns>
+
+        public static VideoJuego Get(int id)
+        {
+            VideoJuego videojuego = null;
+            try
+            {
+                videojuego = VideoJuegoDal.Get(id);
+            }
+            catch (SqlException ex)
+            {
+                Methods.GenerateLogsRelease("VideoJuegoBrl", "Actualizar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Methods.GenerateLogsRelease("VideoJuegoBrl", "Actualizar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+
+            return videojuego;
+        }
     }
 }
