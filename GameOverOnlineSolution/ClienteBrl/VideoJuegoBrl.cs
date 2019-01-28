80e129a7d4888b2529edc51059c2612009df4e3d
desarrollo789 <43588250+desarrollo789@users.noreply.github.com>
2019-01-28 08:48:06 -0600

    Brl videojuego

---
+++ GameOverOnlineSolution/ClienteBrl/VideoJuegoBrl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using Comun;
+using ClienteDal;
+
+namespace ClienteBrl
+{
+    class VideoJuegoBrl
+    {
+        /// <summary>
+        /// Metodo que sirve para insertar a la base de datos
+        /// </summary>
+        /// <param name="videojuego">Objeto brl</param>
+        public static void Insertar(VideoJuego videojuego)
+        {
+            try
+            {
+                VideoJuegoDal.Insertar(videojuego);
+            }
+            catch (SqlException ex)
+            {
+                Methods.GenerateLogsRelease("VideoJuegoBrl", "Insertar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Methods.GenerateLogsRelease("VideoJuegoBrl", "Insertar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Eliminar Paciente
+        /// </summary>
+        /// <param name="id">Identificador del videojuego</param>
+    }
+}
---
+++ GameOverOnlineSolution/GameOverOnlineSolution/Models/PasswordModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace GameOverOnlineSolution.Models
+{
+    public class PasswordModel
+    {
+        #region Propiedades
+        /// <summary>
+        /// rescata al modelo el password encriptado
+        /// </summary>
+        public static string Encriptar{ get; set; }
+        /// <summary>
+        /// rescata al modelo el password desencriptado
+        /// </summary>
+        public static string DesEncriptar { get; set; }
+        #endregion
+    }
+}
