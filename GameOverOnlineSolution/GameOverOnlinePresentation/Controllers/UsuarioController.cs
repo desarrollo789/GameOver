665c97db10f53acdd8f63276acb8d7011a36028d
GhostSergal <44811028+GhostSergal@users.noreply.github.com>
2019-01-31 09:46:29 -0400

    Controllers

---
+++ GameOverOnlineSolution/GameOverOnlinePresentation/Controllers/UsuarioController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameOverOnlinePresentation.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            CargarSexo();
+            UsuarioModel usuario = new UsuarioModel();
+            return View(usuario);
+        }
+
+        public void CargarSexo()
+        {
+            ViewBag.ListaSexos = new SelectList(
+            (
+                from t in SexoListController.SexoModel
+                select new SelectListItem
+                {
+                    Text = t.NombreSexo,
+                    Value = t.SexoId.ToString()
+                }
+            )
+            , "Value", "Text");
+        }
+        // POST: Usuario/Create
+        [HttpPost]
+        public ActionResult Create(UsuarioModel model)
+        {
+            CargarSexo();
+            Guid id = Guid.NewGuid();
+            try
+            {
+                // TODO: Add insert logic here
+                Usuario usuario = new Usuario()
+                {
+
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
