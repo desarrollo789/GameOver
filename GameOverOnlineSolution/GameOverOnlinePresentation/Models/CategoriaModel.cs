4a1c6f07486c61220c26211471ed57d6aaba72df
Comando777 <44825428+Comando777@users.noreply.github.com>
2019-01-31 14:45:30 -0400

    Models

---
+++ GameOverOnlineSolution/GameOverOnlinePresentation/Models/CategoriaModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameOverOnlinePresentation.Models
+{
+    public class CategoriaModel
+    {
+        #region Propiedades
+        /// <summary>
+        /// Recuperacion del codigo dentro del modulo
+        /// </summary>
+
+        [Display(Name = "CategoryId")]
+        public int CategoriaId
+        {
+            get;
+            set;
+        }
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Resource.Resource))]
+        [Display(Name = "FirstName")]
+        [StringLength(50, ErrorMessageResourceName = "FirstNameMessageError", MinimumLength = 3, ErrorMessageResourceType = typeof(Resource.Resource))]
+        public string Nombre
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
---
+++ GameOverOnlineSolution/GameOverOnlinePresentation/Models/VentaDetalleModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameOverOnlinePresentation.Models
+{
+    public class VentaDetalleModel
+    {
+        #region Propiedades
+        /// <summary>
+        /// Recuperacion del codigo dentro del modulo
+        /// </summary>
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Resource.Resource))]
+        [Display(Name = "SellDetailId")]
+        [StringLength(50, ErrorMessageResourceName = "SellDetailIdMessageError", MinimumLength = 3, ErrorMessageResourceType = typeof(Resource.Resource))]
+        public string VentaDetalleId
+        {
+            get;
+            set;
+        }
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Resource.Resource))]
+        [Display(Name = "VDetalle")]
+        [StringLength(50, ErrorMessageResourceName = "VDetalleMessageError", MinimumLength = 3, ErrorMessageResourceType = typeof(Resource.Resource))]
+        public string VDetalle
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
