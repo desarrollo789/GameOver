25d2856dc5992d93612fef4c7d1face7bb887704
GhostSergal <44811028+GhostSergal@users.noreply.github.com>
2019-01-31 15:31:29 -0400

    VideoJuego,Sexo Controllers

---
+++ GameOverOnlineSolution/GameOverOnlinePresentation/Controllers/KeyValueUsuarioListController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GameOverOnlinePresentation.Models;
+
+namespace GameOverOnlinePresentation.Controllers
+{
+    public class KeyValueUsuarioListController : Controller
+    {
+        //GET: KeyValueUsuarioList/
+
+        public ActionResult Index()
+        {
+            KeyValueUsuarioListModel list = KeyValueUsuarioListModel.Get();
+            return View(list);
+        }
+    }
+}
---
+++ GameOverOnlineSolution/GameOverOnlinePresentation/Controllers/SexoListController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClienteBrl;
+using GameOverOnlinePresentation.Models;
+
+namespace GameOverOnlinePresentation.Controllers
+{
+    public class SexoListController : Controller
+    {
+        private static List<SexoModel> _sexoModel;
+
+        public static List<SexoModel> SexoModel
+        {
+            get
+            {
+
+                _sexoModel = new List<SexoModel>();
+                foreach (var sexo in SexoListBrl.Get())
+                {
+                    _sexoModel.Add(new SexoModel
+                    {
+                        SexoId = sexo.SexoId,
+                        Nombre = sexo.Nombre,
+                    });
+                }
+                return _sexoModel;
+            }
+            set { _sexoModel = value; }
+        }
+
+        //
+        // GET: /SexoList/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
